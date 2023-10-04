3fd5893ec8f0fc4810d0eb6c8134299e380f7a63
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-04 06:41:34 -0700

    betty fixed

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -17,8 +17,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	/* Calculating the number of nodes with at least 1 child*/
 	if (tree->left != NULL || tree->right != NULL)
-		res_nodes = 1+ binary_tree_nodes(tree->left) +
-                binary_tree_nodes(tree->right);
+		res_nodes = 1 + binary_tree_nodes(tree->left) +
+			binary_tree_nodes(tree->right);
 
 	/* Returning the result */
 	return (res_nodes);
