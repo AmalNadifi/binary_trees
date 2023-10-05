38ece87d966d948683833625e3bb1285f5f816ea
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-05 09:51:44 -0700

    more fixes

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,30 +9,23 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_root = tree->right;
+	binary_tree_t *new_root;
 
 	/* Check if tree/right child is NULL no rotation can be done*/
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
 
+	new_root = tree->right;
 	/* Updating the right child of the root to be the left child*/
 	tree->right = new_root->left;
 
 	/* Updating the parent pointer of the new left child */
 	if (new_root->left != NULL)
-		new_root->parent = tree;
+		new_root->left->parent = tree;
 
 	/* Updating the parent pointer of the new root to original rootparent*/
 	new_root->parent = tree->parent;
 
-	/* Updating the Parent's child pointer to point to the new root*/
-	if (tree->parent != NULL)
-	{
-		if (tree == tree->parent->left)
-			tree->parent->left = new_root;
-		else
-			tree->parent->right = new_root;
-	}
 	/* Making the original root the left child of the new root*/
 	new_root->left = tree;
 	tree->parent = new_root;
